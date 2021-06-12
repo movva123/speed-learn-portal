d3220f3808b1c47aa25a731a50432f1e9a88540a
movva.ramakrishna@gmail.com <movva.ramakrishna@gmail.com>
2021-06-12 17:31:58 +0530

    commited changes

--- src/App.js
+++ src/App.js
@@ -1,26 +1,26 @@
-import "./App.css";
-import React from "react";
-import "bootstrap/dist/css/bootstrap.css";
+import './App.css';
+import React from 'react';
+import 'bootstrap/dist/css/bootstrap.css';
 
-import "bootstrap/dist/css/bootstrap.min.css";
-import QuestionTemplate from "./components/QuestionTemplate";
+import 'bootstrap/dist/css/bootstrap.min.css';
+import QuestionTemplate from './components/QuestionTemplate';
+import Data from './data.json';
 
-import Data from "./data.json";
 let readQuestion = true;
 const App = () => {
-  return (
-    <div>
-      <QuestionTemplate
-        teacher={Data[0].teacher}
-        school={Data[0].school}
-        assignment={Data[0].assignment}
-        qno={Data[0].qno}
-        progress={Data[0].progress}
-        question={Data[0].question}
-        additional={Data[0].additional}
-      />
-    </div>
-  );
+	return (
+		<div>
+			<QuestionTemplate
+				teacher={Data[0].teacher}
+				school={Data[0].school}
+				assignment={Data[0].assignment}
+				qno={Data[0].qno}
+				progress={Data[0].progress}
+				question={Data[0].question}
+				additional={Data[0].additional}
+			/>
+		</div>
+	);
 };
 
 export default App;
