63065eff98146aa9b700951d50abbf0830215f35
movva.ramakrishna@gmail.com <movva.ramakrishna@gmail.com>
2021-06-12 14:13:47 +0530

    comitted after data shifted to json

--- src/App.js
+++ src/App.js
@@ -1,37 +1,26 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
+import React from "react";
+import "bootstrap/dist/css/bootstrap.css";
 
-import Header from './components/Header'
-import QImage from './components/QImage'
-import Concept from './components/Concept'
+import "bootstrap/dist/css/bootstrap.min.css";
+import QuestionTemplate from "./components/QuestionTemplate";
 
-import React from 'react'
-import 'bootstrap/dist/css/bootstrap.css';
-import ProgressBar from 'react-bootstrap/ProgressBar';
-import Image from 'react-bootstrap/Image';
-import Card from 'react-bootstrap/Card';
-import CustomNavbar from './components/CustomNavbar';
-import { CountdownCircleTimer } from 'react-countdown-circle-timer'
-import FigureImage from 'react-bootstrap/FigureImage'
-import Figure from 'react-bootstrap/Figure';
-import { useState } from 'react';
-import Button from 'react-bootstrap/Button';
-import 'bootstrap/dist/css/bootstrap.min.css'
-import QuestionTemplate from './components/QuestionTemplate';
-let readQuestion=true
-
+import Data from "./data.json";
+let readQuestion = true;
 const App = () => {
- 
   return (
-<div>
-<QuestionTemplate teacher="Mike Scott" school="ABC School -XII class" assignment="Biology-Cellular Structure"  qno="Question No: 6" progress="Progress: 60%" question="What part of mitochondria does the electron transport chain take place?"
- additional="Mitochondria are membrane-bound cell organelles (mitochondrion, singular) that generate most of the chemical energy 
- needed to power the cells biochemical reactions. 
- Chemical energy produced by the mitochondria is stored in a small molecule called adenosine triphosphate (ATP)."/>
-</div>
-  )
-}
+    <div>
+      <QuestionTemplate
+        teacher={Data[0].teacher}
+        school={Data[0].school}
+        assignment={Data[0].assignment}
+        qno={Data[0].qno}
+        progress={Data[0].progress}
+        question={Data[0].question}
+        additional={Data[0].additional}
+      />
+    </div>
+  );
+};
 
-export default App
-
-
+export default App;
